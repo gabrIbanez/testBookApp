fe584779e0b5efeff51c8c054a2ff988b94584b7
gabrielibanez <g.ibanezw@gmail.com>
2018-08-23 17:09:38 +0200

    begun seracing pg

--- testBookApp/app/models/book.rb
+++ testBookApp/app/models/book.rb
@@ -1,8 +1,15 @@
 class Book < ApplicationRecord
-	after_create :author_books_implementation
-
-	belongs_to :author 
+	include PgSearch
+  pg_search_scope :search_by_name, 
+  	against: :name,
+  	associated_against: {
+  		author: [:first_name, :last_name]
+  	}
+  		
 	
+	after_create :author_books_implementation
+	
+	belongs_to :author 
 
 	def author_books_implementation
 		self.author.update(nb_of_book_published: self.author.books.count)
