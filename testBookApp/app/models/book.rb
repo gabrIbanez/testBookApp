948c5c4ab74593ec31988f02a0e0dd9e9bd5166d
gabrielibanez <g.ibanezw@gmail.com>
2018-08-23 16:55:00 +0200

    pg search out

--- testBookApp/app/models/book.rb
+++ testBookApp/app/models/book.rb
@@ -2,12 +2,7 @@ class Book < ApplicationRecord
 	after_create :author_books_implementation
 
 	belongs_to :author 
-	include PgSearch
-  pg_search_scope :search_by_title_and_syllabus,
-    against: [ :name, :author_name, :author ],
-    using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
-    }
+	
 
 	def author_books_implementation
 		self.author.update(nb_of_book_published: self.author.books.count)
